764edddb5a38610e00c5022d064e8467bd6c6176
clark <huajie.chen@utoronto.edu>
2014-06-04 21:23:00 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
  
 if( isset($_POST['message']) ) {
  
-	$endpoint = "https://clarktodlist.azure-mobile.net/tables/TodoItem";
+	$endpoint = "https://clarktodolist.azure-mobile.net/tables/TodoItem";
 	 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $endpoint);
