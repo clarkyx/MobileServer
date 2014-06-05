0a19c667684fa3c4dd277ecef9f269c43bbb18f8
clark <huajie.chen@utoronto.ca>
2014-06-04 20:57:38 -0400

    new things here

--- Index.php
+++ Index.php
@@ -1,30 +1,36 @@
 <?php
-	if(isset($_POST['message'])){
-		$endpoint = "http://clarktodolist.azure-mobile.net/tables/Messages";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $endpoint);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'X-ZUMO-APPLICATION: MIajyClrhzybAIQfCAheCzhgmhxAYC49',
-			'Accept: application/json',
-			'Content-Type: application/json',
-			)
-		);
-
-	$json = "{ \ "Text\": \ "".$_POST['message']."\"}";
+ 
+if( isset($_POST['message']) ) {
+ 
+	$endpoint = "https://YOUR-MOBILE-SERVICE-NAME.azure-mobile.net/tables/Messages";
+	 
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $endpoint);
+	 
+	// Set so curl_exec returns the result instead of outputting it.
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	    'X-ZUMO-APPLICATION: YOUR KEY HERE',
+	    'Accept: application/json',
+	    'Content-Type: application/json',
+	    ));	
+	 
+	$json = "{ \"Text\": \"".$_POST['message']."\" }";
 	echo $json;
-
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+	 
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $json);
+	// Get the response and close the channel.
 	$response = curl_exec($ch);
-
+	curl_close($ch);
+	 
 	echo $response;
-	}
-
+}
+ 
 ?>
-
+ 
 <form method="post" action="messages.php">
 <textarea name="message"></textarea>
-<input type="submit" value= "Invia"/>
+<input type="submit" value="Invia"/>
 </form>
